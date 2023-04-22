d4a5901dc2b2566d715c073a189958776a6a973f
22222 11111 <vanya.popov.2003@gmail.com>
2023-04-22 11:01:45 +0300

    Deleted code

--- LabMO4/LabFuctions.cs
+++ LabMO4/LabFuctions.cs
@@ -12,18 +12,6 @@ namespace LabMO4
         {
             return 8*Math.Pow(x.x1,2)+Math.Pow(x.x2,2)-4;
         }
-        public static double GetMainFunction(MyVector x, double r)
-        {
-            return GetDefaultFunction(x)+r/2*Math.Pow(x.x1+3*x.x2+6,2);
-        }
-        public static double GetX1Grad(MyVector x, double r)
-        {
-            return 16 * x.x1 + r * (x.x1 + 3 * x.x2 + 6);
-        }
-        public static double GetX2Grad(MyVector x, double r)
-        {
-            return 2 * x.x1 + 3 * r * (x.x1 + 3 * x.x2 + 6);
-        }
         public static double GetPFunction(MyVector x,double r)
         {
             return r/2*Math.Pow(x.x1+3*x.x2+ 6,2);
